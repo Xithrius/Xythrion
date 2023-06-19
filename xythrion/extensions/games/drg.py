690df0f375098674dee2ab73c42ba57c31d3e60c
Xithrius <xithrius@gmail.com>
2023-06-19 05:58:27 -0700

    Added croniter dependency, drg weekly resets complete

--- xythrion/extensions/games/drg.py
+++ xythrion/extensions/games/drg.py
@@ -1,12 +1,12 @@
 from datetime import datetime, timedelta, timezone
 
+from croniter import croniter
 from discord.ext.commands import Cog, group
 
 from xythrion.bot import Xythrion
 from xythrion.context import Context
 
-WEEKDAY_INDEX_RESET = 3
-HOUR_RESET = 11
+WEEKLY_CRON = "00 11 * * THU"
 
 
 class DeepRockGalactic(Cog):
@@ -33,22 +33,13 @@ class DeepRockGalactic(Cog):
         """
         now = datetime.now(tz=self.tzinfo)
 
-        days_ahead = WEEKDAY_INDEX_RESET - now.weekday()
-        hours_ahead = HOUR_RESET - now.hour
+        next_thursday = croniter(WEEKLY_CRON, now).get_next(datetime)
 
-        if days_ahead <= 0:
-            days_ahead += 7
-
-        if hours_ahead <= 0:
-            hours_ahead += 24
-
-        delta = now + timedelta(days=days_ahead, hours=hours_ahead)
-
-        return int(delta.timestamp())
+        return int(next_thursday.timestamp())
 
     @drg.command(aliases=("weekly",))
     async def next_weekly(self, ctx: Context) -> None:
         """Time delta until the weekly quests reset."""
-        weekly_timestamp = self.next_weekday()
+        weekly_timestamp = self.next_reset()
 
         await ctx.send(f"Next weekly reset is in <t:{weekly_timestamp}:R>")
