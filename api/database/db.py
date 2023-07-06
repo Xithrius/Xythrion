1e905ea770f575ad0656c5a5ec0b6dd27608fc82
JacobMonck <jacobamonck@gmail.com>
2023-07-06 19:15:42 -0400

    refactor: add parent meta class

--- api/database/db.py
+++ api/database/db.py
@@ -1,7 +1,15 @@
 from os import environ
 
 from databases import Database
+from ormar import ModelMeta
 from sqlalchemy import MetaData
 
 database = Database(environ["DB_URI"])
 metadata = MetaData()
+
+
+class ParentMeta(ModelMeta):
+    """Base meta class for all Ormar models."""
+
+    database = database
+    metadata = metadata
