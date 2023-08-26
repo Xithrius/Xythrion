d8f4e66b90c49313242558bdcf23be960e74ffa8
Xithrius <xithrius@gmail.com>
2023-08-25 23:17:39 -0700

    Formatted everything

--- site/app/error.tsx
+++ site/app/error.tsx
@@ -1,18 +1,12 @@
-'use client'
+"use client";
 
-import { useEffect } from 'react'
+import { useEffect } from "react";
 
-export default function Error({
-  error,
-  reset,
-}: {
-  error: Error
-  reset: () => void
-}) {
+export default function Error({ error, reset }: { error: Error; reset: () => void }) {
   useEffect(() => {
     // Log the error to an error reporting service
-    console.error(error)
-  }, [error])
+    console.error(error);
+  }, [error]);
 
   return (
     <div>
@@ -26,5 +20,5 @@ export default function Error({
         Try again
       </button>
     </div>
-  )
+  );
 }
