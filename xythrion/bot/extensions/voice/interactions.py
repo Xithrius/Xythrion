573cefa7e8c83e45635340edc9ce58c9f0b11349
Xithrius <xithrius@gmail.com>
2024-01-17 19:37:25 -0800

    Join and leave voice channel events

--- xythrion/bot/extensions/voice/interactions.py
+++ xythrion/bot/extensions/voice/interactions.py
@@ -1,4 +1,6 @@
+from discord import Member, VoiceState
 from discord.ext.commands import Cog, command
+from loguru import logger as log
 
 from bot.bot import Xythrion
 from bot.context import Context
@@ -7,6 +9,25 @@ from bot.context import Context
 class VoiceChannelInteractions(Cog):
     def __init__(self, bot: Xythrion) -> None:
         self.bot = bot
+
+    @Cog.listener()
+    async def on_voice_state_update(self, member: Member, before: VoiceState, after: VoiceState) -> None:
+        bot_voice_channel = None
+        for vc in self.bot.voice_clients:
+            if vc.guild == member.guild:
+                bot_voice_channel = vc.channel
+                break
+
+        if not bot_voice_channel:
+            return
+
+        # Check if a user joins the voice channel the bot is in
+        if after.channel == bot_voice_channel and member != self.bot.user:
+            log.info(f"{member} joined the voice channel {after.channel.name}.")
+
+        # Check if a user leaves the voice channel the bot is in
+        if before.channel == bot_voice_channel and after.channel != bot_voice_channel:
+            log.info(f"{member} left the voice channel {before.channel.name}.")
 
     @command()
     async def join(self, ctx: Context) -> None:
