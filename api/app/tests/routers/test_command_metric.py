9b72d2a2384b65e2d4326dc235edfc29f9714e5b
Xithrius <xithrius@gmail.com>
2024-01-19 23:03:34 -0800

    More test coverage on command metrics

--- api/app/tests/routers/test_command_metric.py
+++ api/app/tests/routers/test_command_metric.py
@@ -6,7 +6,23 @@ from starlette import status
 
 
 @pytest.mark.anyio
-async def test_creation(
+async def test_command_metric_lists_nothing(
+    fastapi_app: FastAPI,
+    client: AsyncClient,
+    dbsession: AsyncSession,
+) -> None:
+    url = fastapi_app.url_path_for("get_all_command_metrics")
+    response = await client.get(url)
+
+    assert response.status_code == status.HTTP_200_OK
+
+    data = response.json()
+
+    assert data == []
+
+
+@pytest.mark.anyio
+async def test_command_metric_creation(
     fastapi_app: FastAPI,
     client: AsyncClient,
     dbsession: AsyncSession,
@@ -18,3 +34,37 @@ async def test_creation(
     }
     response = await client.post(url, json=new_command)
     assert response.status_code == status.HTTP_201_CREATED
+
+
+@pytest.mark.anyio
+async def test_command_metric_creation_and_list(
+    fastapi_app: FastAPI,
+    client: AsyncClient,
+    dbsession: AsyncSession,
+) -> None:
+    url = fastapi_app.url_path_for("create_command_usage_metric")
+    new_command = {
+        "command_name": "test_command",
+        "successfully_completed": True,
+    }
+    response = await client.post(url, json=new_command)
+    assert response.status_code == status.HTTP_201_CREATED
+
+    url = fastapi_app.url_path_for("get_all_command_metrics")
+    response = await client.get(url)
+
+    assert response.status_code == status.HTTP_200_OK
+
+    data = response.json()
+
+    assert len(data) == 1
+
+    d = data[0]
+
+    command_id = d.pop("id")
+    used_at = d.pop("used_at")
+
+    assert command_id
+    assert used_at
+
+    assert d == new_command
