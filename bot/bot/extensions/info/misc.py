19e025ce8debd77871dede510bb09c58902bfde9
Xithrius <xithrius@gmail.com>
2024-06-29 05:59:56 -0700

    Uptime presence background task

--- bot/bot/extensions/info/misc.py
+++ bot/bot/extensions/info/misc.py
@@ -1,8 +1,11 @@
+import asyncio
 from datetime import datetime
 
+import discord
 from discord import Embed
 from discord.ext.commands import Cog, command
 from humanize import naturaldelta
+from loguru import logger as log
 
 from bot.bot import Xythrion
 from bot.context import Context
@@ -12,6 +15,22 @@ from bot.settings import settings
 class Misc(Cog):
     def __init__(self, bot: Xythrion):
         self.bot = bot
+
+        self.bg_presence_uptime_task = self.bot.loop.create_task(self.presence_uptime_task())
+
+    async def presence_uptime_task(self) -> None:
+        await self.bot.wait_until_ready()
+        log.info("Started presence uptime background task")
+
+        while not self.bot.is_closed():
+            current_datetime = datetime.now(self.bot.tzinfo)
+            delta = current_datetime - (self.bot.startup_datetime or current_datetime)
+            human_delta = naturaldelta(delta.total_seconds())
+
+            presence = discord.CustomActivity(f"Up for {human_delta}")
+            await self.bot.change_presence(activity=presence)
+
+            await asyncio.sleep(60)
 
     @command(name="prefix")
     async def get_prefix(self, ctx: Context) -> None:
